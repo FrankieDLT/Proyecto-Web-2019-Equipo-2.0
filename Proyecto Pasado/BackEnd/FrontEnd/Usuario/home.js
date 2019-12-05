03bb184f6314d5ef6c4dd32aaaf1d73ea4047a0b
Alejandro <alex-mora95@hotmail.com>
2019-12-05 15:00:08 -0600

    products working

--- Proyecto Pasado/BackEnd/FrontEnd/Usuario/home.js
+++ Proyecto Pasado/BackEnd/FrontEnd/Usuario/home.js
@@ -2,7 +2,7 @@
 //12 obj
 let url = "https://api.myjson.com/bins/1d05le";
 
-let url2 = 'http://localhost:3000/api/products'
+let url2 = "http://localhost:3000/api/products"
 
 var cart = {
     items: []
@@ -41,23 +41,24 @@ function httpRequest(address, reqType, asyncProc) {
     return req;
 }
 
-$.ajax({
+
+/*$.ajax({
     url: url2,
     type: 'GET',
     //accepts: "application/json",
     //beforeSend: function(xhr){xhr.setRequestHeader('x-auth-user', 'hIGn7uEuPb-5de6deff9694c431845c908e');},
     headers: {
         'Access-Control-Allow-Origin': '*',
-        'x-auth-user':'hIGn7uEuPb-5de6deff9694c431845c908e',
+        'x-auth-user':JSON.parse(localStorage.userToken),
     },
     dataType:JSON,
     success: function(result){ 
         console.log("resultado:",result)
     },
     error: function(error) {
-        console.log(`Error ${"error ", error}`)
+        console.log("error ", error)
     }
-})
+})*/
 
 
 function loadJSON(url, cbOk = callback_ok, cbErr) {
@@ -67,9 +68,14 @@ function loadJSON(url, cbOk = callback_ok, cbErr) {
 
     // 2. Configurar: PUT actualizar archivo
     xhr.open('GET', url);
+    console.log(url);
+    let cualquierMamada = JSON.parse(localStorage.userToken);
+    console.log(cualquierMamada.token);
+    console.log(localStorage.userToken);
     //xhr.setRequestHeader("Content-Type", "application/json"); 
-    //xhr.setRequestHeader("x-auth-user", 'hIGn7uEuPb-5de6deff9694c431845c908e');
+    xhr.setRequestHeader("x-auth-user", cualquierMamada.token);
 
+    console.log('Por llamar');
     // 4. Enviar solicitud
     xhr.send();
 
@@ -97,7 +103,7 @@ let productos = [];
 
 function initData() {
 
-    loadJSON(url,
+    loadJSON(url2,
         cbOk => {
             productos = cbOk;
             console.log(productos);
@@ -116,7 +122,7 @@ function productToHTML(productos) {
         <div class="card-body">
                 <h5 class="card-title">${productos.descripcion}</h5>
                 <p class="card-text">Categoria: ${productos.categoria}</p>
-                <p class="card-text">Stock: ${productos.cantidad}</p>
+                <p class="card-text">Stock: ${productos.stock}</p>
                 <p class="card-text">Disponible: Ahora</p>
 
             <a href="#" class="btn btn-primary" onclick="agregarCarrito(${productos.id})">Agregar al carrito</a>
@@ -152,7 +158,7 @@ function agregarCarrito(id) {
             categoria: productos[id].categoria,
             id: id,
             cantidad: 1,
-            stock: productos[id].cantidad
+            stock: productos[id].stock
         }
         cart.items.push(producto);
         localStorage.setItem('cart', JSON.stringify(cart));
--- Proyecto Pasado/BackEnd/server.js
+++ Proyecto Pasado/BackEnd/server.js
@@ -16,26 +16,10 @@ app.use('/admin/deudores', express.static(__dirname + '/FrontEnd/Administrador/D
 app.use('/home', express.static(__dirname + '/FrontEnd/Usuario/'));
 app.use('/home/carrito', express.static(__dirname + '/FrontEnd/Usuario/carrito.html'));
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
-
+let corsConfig = {
+    origin: "*"
+}
+app.use(cors(corsConfig));
 
 app.use(express.json());
 app.use('/api/users', usersRouter);
